728c8fa0f79136de27ee65db2521cb93d1c853e1
Boyan Bonev <b.bonev@redbuffstudio.com>
2019-03-21 08:59:52 +0200

    Push exercise week 05.

---
+++ week-05/exercise-05.hs
@@ -0,0 +1,29 @@
+-- Lecture 5: Lists. We should know all about lists. DO WE?
+
+
+-- Exercise 1: Let's write a function that accepts two ordered lists and merges them.
+
+--merge [1, 3, 5] [2, 4, 6] =>>> [1, 2, 3, 4, 5, 6]
+
+
+
+
+-- Exercise 2: Write a function accepting a list of Integers and returning a tuple of two integer lists - odd and even.
+
+--spltiPlz [1, 2, 3] =>>> ([1,3], [2])
+
+
+
+
+-- Exercise 3: Let's write a function that checks if a string starts with another,
+
+--isPrefix "cat" "dog" => False
+--isPrefix "ca" "cat" => True
+
+
+
+
+
+-- Exercise 4: Let's write an fn that returns the number of occurrences of the smallest number in it.
+
+--occurrencesMin [1, 2, 3, 4, 5, 1, 1, 1] =>>> 4
